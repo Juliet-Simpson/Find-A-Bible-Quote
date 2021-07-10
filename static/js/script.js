3a5fb644d094c45e96be32354bfacdcbfb6b6b9e
Juliet-Simpson <simpsonjulietc@gmail.com>
2021-07-10 21:45:46 +0000

    Fade out flash message

--- static/js/script.js
+++ static/js/script.js
@@ -2,3 +2,9 @@ $(document).ready(function(){
   $('.sidenav').sidenav({edge: "right"});
   $('.modal').modal();
 });
+
+$(function() {
+  $('#flash').delay(0).fadeIn('normal', function() {
+     $(this).delay(2500).fadeOut();
+  });
+});

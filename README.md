5c101a809a1c779c28335d8612cd8938057ced12
Juliet-Simpson <simpsonjulietc@gmail.com>
2021-07-10 20:33:39 +0000

    Add login elements and functionality

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 sidenav edge right taken from mini project
+
+register bugs.. return what? empty open modal if username already exists (clear form) previous page if registration successful, whatever previous page was.. Make flash messages go away after flashing.   Need to confirm password before making dictionary... How?? notes p53...
+
+CHANGE DUBUG TO FALSE
--- app.py
+++ app.py
@@ -24,6 +24,34 @@ def render_homepage():
     return render_template("homepage.html")
 
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # check if username exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            # ensure hashed password matches user input
+            if check_password_hash(
+                    existing_user["password"], request.form.get("password")):
+                        session["user"] = request.form.get("username").lower()
+                        flash("Welcome, {}".format(
+                            request.form.get("username")))
+                        return render_template("base.html")
+            else:
+                # invalid password match
+                flash("Incorrect Username and/or Password")
+                return render_template("base.html")
+
+        else:
+            # username doesn't exist
+            flash("Incorrect Username and/or Password")
+            return render_template("base.html")
+
+    return render_template("base.html")
+
+
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
@@ -44,7 +72,6 @@ def register():
         session["user"] = request.form.get("username").lower()
         flash("Registration Successful!")
         return render_template("base.html")
-
 
     return render_template("base.html")
 
